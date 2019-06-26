d37ecbb10dd2c3c16ad80ac0db9dce6bf3309194
Saaim Huda <saaim-huda@users.noreply.github.com>
2019-06-25 23:25:10 -0400

    added exception

---
+++ src/main/java/com/revature/exception/InvalidUsernameException.java
@@ -0,0 +1,14 @@
+package com.revature.exception;
+
+public class InvalidUsernameException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public InvalidUsernameException() {
+
+	}
+
+}
